d902af3e319187f80cd058bf638c2aa6ca3c163f
Amatullah Brown <amatullahbrown@Amatullahs-MBP.fios-router.home>
2020-12-21 20:29:42 -0500

    updated chant

--- WuBuzz.js
+++ WuBuzz.js
@@ -1,6 +1,20 @@
 class WuBuzz {
     wuChant(value){
-        return null;
+        for (let i=1; i<=value; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                console.log("WuTangForever");
+            }
+            else if (i % 3 == 0) {
+                console.log("Wu");
+            }
+            else if (i % 5 == 0) {
+                console.log("Tang");
+            }
+            else {
+                console.log(i);
+            }
+        }
+        return;
     }
 }
 
