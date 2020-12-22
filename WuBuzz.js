71f26593a0ed4cdf2abf7f577b9e0d63c6c14539
Amarachukwu Ugoanusi <nkanusi@gmail.com>
2020-12-22 13:46:42 -0500

    first commit for Javescript lab 1

--- WuBuzz.js
+++ WuBuzz.js
@@ -1,8 +1,14 @@
-class WuBuzz {
-    wuChant(value){
-        return null;
-    }
+// class WuBuzz {
+//     wuChant(value){
+//         return null;
+//     }
+// }
+
+
+// module.exports = WuBuzz;
+
+let number = [1,2,[3,'Wu'],4,[5,'Tang'],[6,'Wu'],7,8,[9,'Wu'],[10,'Tang'],11,12,13,14,[15,'WuTang Forever']];
+for (let i=0 ; i <number.length; i++){
+    console.log(number[i][1]);
+
 }
-
-
-module.exports = WuBuzz;
